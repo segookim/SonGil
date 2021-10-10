1180aa853fd028e763ebdd5198e0d32caaf7d294
segookim <>
2021-10-10 17:22:25 +0900

    20211010 homename

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,12 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import About from './layouts/About/About';
 import Explain from './layouts/Explain';
 import NotFound from './layouts/NotFound';
-import RealTimeObjectDetection from './songil/RealTimeObjectDetection';
+import Home from './songil/Home';
 
 export default ({ childProps }) =>
   <Switch>
     <Route path="/Explain" exact component={Explain} props={childProps} />
     <Route path="/about" exact component={About} props={childProps} />
-    <Route path="/" exact component={RealTimeObjectDetection} props={childProps} />
+    <Route path="/" exact component={Home} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
