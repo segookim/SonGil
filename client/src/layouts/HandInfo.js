e4fe045a0d9ffb8fba51ff450deca9069c97cb81
HaeSeong Yang <yanghs632@gmail.com>
2021-10-30 23:37:16 +0900

    211030 UI responsive

--- client/src/layouts/HandInfo.js
+++ client/src/layouts/HandInfo.js
@@ -1,7 +1,6 @@
 import React from "react";
 // import { Collapse } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
-import you from '../images/너.jpg';
 // import Tensorflowjsimg from '../images/Tensorflowjsimg.jpg'
 
 const useStyles = makeStyles(() => ({
@@ -58,15 +57,15 @@ const HandInfo = () => {
     // const images = [{"you": '../images/너.jpg'}]
 
     const handInfos = [
-        {"shape": "오른 주먹의 1지를 펴서 끝이 밖으로 향하게 하여 약간 내민다.", "image": "너", "meaning": "너"},
-        // {"shape": "오른 주먹의 1지를 펴서 끝이 밖으로 향하게 하여 약간 내민다.", "image": "너", "meaning": "너"},
+        {"shape": "오른 주먹의 1지를 펴서 끝이 밖으로 향하게 하여 약간 내민다.", "image": "you", "meaning": "너"},
+        {"shape": "오른손의 1·5지 끝을 맞대어 동그라미를 만들어 좌우로 가볍게 두 번 흔든다.", "image": "money", "meaning": "돈"},
     ]
 
     const handList = handInfos.map((handInfo) => (
         // import you from {handInfo.image}
         <Collapse title={handInfo.meaning}>
             {handInfo.shape}
-            <img src={require(`../images/${handInfo.image}.jpg`)}></img>
+            <img src={`./hand/${handInfo.image}.jpg`} style={{width: "30vw"}}/>
         </Collapse>
     ));
 
@@ -74,7 +73,6 @@ const HandInfo = () => {
 
     return(
         <div className={classes.root}>
-            <img src={you}/>
             <h1>지원 수어 살펴보기</h1>
             <div>
                 {handList}
