3021b4833ac2b08f7f5810c440b09f91e4ac8230
HaeSeong Yang <yanghs632@gmail.com>
2021-11-16 00:09:44 +0900

    211115 home UI litte change

--- client/src/songil/SignToText.js
+++ client/src/songil/SignToText.js
@@ -117,7 +117,7 @@ function SignToText({ setMessages }) {
           marginRight: "auto",
           zindex: 9,
           width: "100%",
-          height: "26vw",
+          height: "45vh",
         }}
       />
     </div>
